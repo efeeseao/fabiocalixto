8e600586a1d1ef5534212d5df9588f6da30346a1
Fernando dos Santos <nando4803@gmail.com>
2026-05-18 09:46:48 +0100

    feat: add OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,14 @@ export const metadata: Metadata = {
     siteName: "Fábio Calixto",
     locale: "pt_PT",
     type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Fábio Calixto",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
